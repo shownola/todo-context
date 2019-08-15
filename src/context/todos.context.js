aeef8853a6a1538e761e4366dd35e1d5a885858d
shownola <swasieko@gmail.com>
2019-08-15 14:28:25 -0700

    add todos context

---
+++ src/context/todos.context.js
@@ -0,0 +1,17 @@
+import React, {createContext} from 'react';
+import useTodoState from '../hooks/useTodoState';
+
+const defaultTodos = [
+  {id: 1, task: 'Mow the lawn using goats', complete: false},
+  {id: 2, task: 'Release lady bugs into garden', completed: true}
+];
+export const TodosContext = createContext();
+
+export function TodosProvider(props){
+  const todoStuff = useTodoState(defaultTodos);
+  return(
+    <TodosContext.Provider value={todoStuff}>
+      {props.children}
+    </TodosContext.Provider>
+  )
+}
