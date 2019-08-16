51dc0c9a83282611c826a48c30361fc5dc9f7647
shownola <swasieko@gmail.com>
2019-08-15 17:47:48 -0700

    Add reducer

--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -1,5 +1,7 @@
-import React, {createContext} from 'react';
+import React, {createContext, useReducer} from 'react';
 import useTodoState from '../hooks/useTodoState';
+import todoReducer from '../reducers/todo.reducer';
+
 
 const defaultTodos = [
   {id: 1, task: 'Mow the lawn using goats', complete: false},
@@ -8,9 +10,9 @@ const defaultTodos = [
 export const TodosContext = createContext();
 
 export function TodosProvider(props){
-  const todoStuff = useTodoState(defaultTodos);
+  const [todos, dispatch] = useReducer(todoReducer, defaultTodos)
   return(
-    <TodosContext.Provider value={todoStuff}>
+    <TodosContext.Provider value={{todos, dispatch}}>
       {props.children}
     </TodosContext.Provider>
   )
